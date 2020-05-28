09e8c505472abd36e5f4ed6779743d36c96b1e53
Gautam <gramasu000@citymail.cuny.edu>
2020-05-28 10:24:29 -0400

    Moved drop statements to another sql file

--- init.sql
+++ init.sql
@@ -1,27 +1,19 @@
-create schema if not exists p1; 
+create schema if not exists p1;
 
-create table if not exists p1.employee (
-  id int primary key,
-  firstname varchar(20) not null,
-  lastname varchar(30) not null,
-  email varchar(20) unique not null,
-  emp_username text,
-  emp_password text,
-  emp_type text not null,
-  unique not null (emp_username, emp_password)
-);
+create table if not exists p1.employee ( id int primary key,
+firstname varchar(20) not null,
+lastname varchar(30) not null,
+email varchar(20) unique not null,
+emp_username text not null,
+emp_password text not null,
+emp_type text not null,
+unique (emp_username, emp_password) );
 
-create table if not exists p1.request (
-  id int primary key,
-  type text not null,
-  amount int not null,
-  empid references p1.employee(id),
-  dsid references p1.employee(id),
-  dhid references p1.employee(id),
-  bcid references p1.employee(id),
-  status text
-);
-
-drop table p1.employee;
-drop table p1.request;
-drop schema p1;
+create table if not exists p1.request ( id int primary key,
+type text not null,
+amount int not null,
+empid int references p1.employee(id),
+dsid int references p1.employee(id),
+dhid int references p1.employee(id),
+bcid int references p1.employee(id),
+status text );
---
+++ reset.sql
@@ -0,0 +1,5 @@
+drop table if exists p1.request;
+
+drop table if exists p1.employee;
+
+drop schema if exists p1;
