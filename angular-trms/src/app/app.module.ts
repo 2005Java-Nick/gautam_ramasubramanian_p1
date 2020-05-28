d336236056891606506de3513f8a8c3fb9415c90
Gautam <gramasu000@citymail.cuny.edu>
2020-05-27 21:42:46 -0400

    Basic UI for Signup Page

--- angular-trms/src/app/app.module.ts
+++ angular-trms/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ angular-trms/src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Option } from '../option-interface';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+
+  header : string = "Create Your Account";
+
+  employeeOptions : Option[] = [
+    { name: 'employee', label: 'Regular Employee' },
+    { name: 'direct-supervisor', label: 'Supervisor' },
+    { name: 'department-head', label: 'Department Head' },
+    { name: 'benefits-coordinator', label: 'Benefits Coordinator' }
+  ];
+
+  signupForm : FormGroup = this.formBuilder.group({
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    email: [null, Validators.required],
+    username: [null, Validators.required],
+    password: [null, Validators.required],
+    empType: [null, Validators.required]
+  });
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
