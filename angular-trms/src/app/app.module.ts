c9620b35f2a2422391881406de3a56a0160c5d38
Gautam <gramasu000@citymail.cuny.edu>
2020-05-27 22:49:51 -0400

    Styled page so that it has dark blue theme

--- angular-trms/src/app/app.module.ts
+++ angular-trms/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupFormComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
