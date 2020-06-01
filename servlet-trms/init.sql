94f45c8b2311c7ee89dc2e1435340cc0596552c4
Gautam <gramasu000@citymail.cuny.edu>
2020-06-01 15:47:46 -0400

    Added comments to init.sql

--- servlet-trms/init.sql
+++ servlet-trms/init.sql
@@ -1,5 +1,19 @@
+/********************************************************************
+ * Project One Postgres Database Initialization Script
+ * 
+ * Task - Set up all schemas, tables, functions and stored procedures
+ * 			needed for project one
+ * 
+ ********************************************************************/
+
+/********************************************************************
+ * Create Project 1 Schema
+ ********************************************************************/
 create schema if not exists p1;
 
+/********************************************************************
+ * Create an Employee Table inside Project 1 Schema
+ ********************************************************************/
 create table if not exists p1.employee ( 
   empId bigserial primary key,
   empFirstName varchar(20) not null,
@@ -21,9 +35,18 @@ create table if not exists p1.employee (
   empBenCo int
 );
 
+/********************************************************************
+ * Set Email, Phone Number and Username to be unique entries
+ ********************************************************************/
 alter table p1.employee add constraint u_empEmail unique(empEmail);
 alter table p1.employee add constraint u_empPhone unique(empPhone);
 alter table p1.employee add constraint u_username unique(empUsername);
+
+
+/********************************************************************
+ * Every Employee has a Direct Supervisor, a Department Head
+ * 	and a Benefits Coordinator
+ ********************************************************************/
 alter table p1.employee add constraint fk_empDirectSup foreign key (empDirectSup) 
   references p1.employee(empId) on delete no action on update no action;
 alter table p1.employee add constraint fk_empDeptHead foreign key (empDeptHead)
@@ -31,6 +54,9 @@ alter table p1.employee add constraint fk_empDeptHead foreign key (empDeptHead)
 alter table p1.employee add constraint fk_empBenCo foreign key (empBenCo)
   references p1.employee(empId) on delete no action on update no action;
 
+/********************************************************************
+ * Create a Event Table in the Project 1 Schema
+ ********************************************************************/
 create table if not exists p1.reimbursedEvent (
   eventId bigserial primary key,
   eventType varchar(30) not null,
@@ -46,7 +72,9 @@ create table if not exists p1.reimbursedEvent (
   eventReimbursementPercent numeric(2,0)
 );
 
-
+/********************************************************************
+ * Create a Reimbursement Request Table in Project 1 Schema
+ ********************************************************************/
 create table if not exists p1.reimbursementRequest ( 
   rrId bigserial primary key,
   rrRequester int,
@@ -60,12 +88,19 @@ create table if not exists p1.reimbursementRequest (
   rrGrade text
 );
 
+/********************************************************************
+ * Every Reimbursement Request is tied to an employee and an event
+ ********************************************************************/
 alter table p1.reimbursementRequest add constraint fk_rrRequester foreign key (rrRequester)
   references p1.employee(empId) on delete no action on update no action;
 
 alter table p1.reimbursementRequest add constraint fk_rrEvent foreign key (rrEvent)
   references p1.reimbursedEvent(eventId) on delete no action on update no action;
 
+/********************************************************************
+ * Create a File Table for all uploaded binary files 
+ * 	They will be stored as hexadecimal strings
+ ********************************************************************/
 create table if not exists p1.file (
   fileId bigserial primary key,
   fileName text,
@@ -73,9 +108,26 @@ create table if not exists p1.file (
   fileRR int
 );
 
+/********************************************************************
+ * Make foreign key in File table to Reimbursement Request table
+ ********************************************************************/
 alter table p1.file add constraint fk_fileRR foreign key (fileRR)
   references p1.reimbursementRequest(rrId) on delete no action on update no action;
 
+/********************************************************************
+ * Create Table for Approval, Info and Change "Requests"
+ * 	(I call them requests but they are not the same as 
+ * 		reimbursement requests)
+ * An Approval Request is an entry that states that an employee
+ * 	requested the direct supervisor (or Dept Head or Benefits Coordinator) 
+ * 	to look over his/her reimbursement request and approve it
+ * An Info Request is an entry that states that the supervisor
+ * 	(or Dept Head or Benefits Coordinator) requested the employee
+ * 	for more information before he/she could approve the reimbursement request
+ * A Change Request is an entry that states that the supervisor
+ * 	(or Dept Head or Benefits Coordinator) requested the employee
+ * 	to change something in his/her reimbursement request before approval
+ ********************************************************************/
 create table if not exists p1.approvalInfoChangeRequest (
   aicId bigserial primary key,
   aicRR int,
@@ -87,6 +139,11 @@ create table if not exists p1.approvalInfoChangeRequest (
   aicMessage text
 );
 
+/********************************************************************
+ * Every AIC (Approval Request, Info Request, Change Request) 
+ * 	is attached to a Reimbursement Request.
+ * It is initiated by an employee and is directed to another employee
+ ********************************************************************/
 alter table p1.approvalInfoChangeRequest add constraint fk_aicRR foreign key (aicRR)
   references p1.reimbursementRequest(rrId) on delete no action on update no action;
 
@@ -97,6 +154,12 @@ alter table p1.approvalInfoChangeRequest add constraint fk_aicTo foreign key (ai
   references p1.employee(empId) on delete no action on update no action;
  
 
+/********************************************************************
+ * Helper Function - Not to be called directly but used in another function.
+ * This outputs an integer that marks whether the employee id (called id)
+ * 	is that of a employee, a supervisor, a department head 
+ * 	or a benefits coordinator.
+ ********************************************************************/
 create or replace function p1.getRequestOrder(id bigint, eid int, sid int, hId int, bId int) 
   returns int
   as $$
@@ -110,7 +173,13 @@ create or replace function p1.getRequestOrder(id bigint, eid int, sid int, hId i
     end;
   $$ language plpgsql;
 
-
+/********************************************************************
+ * Function getRequestHierarchy
+ * 	Takes in as input the employee username and password
+ * 	and outputs the details of the employee, his/her supervisor, 
+ * 		the department head and the benefits coordinator
+ * 		in that order (using the helper function above).
+ ********************************************************************/
 create or replace function p1.getRequestHierarchy(argUsername text, argPassword text) 
 	returns table (empId bigint, empFirstName varchar(20), empLastName varchar(30), empEmail varchar(50),
   empUsername text, empPassword text, empPosition varchar(50), empHireDate date, empBirthDate date,
